61fc0f92f1ea9076ba9f9c0e5abbc3b84d5d9a87
Umair Afzal <cumair600@gmail.com>
2019-09-13 00:18:52 +0500

    "Added Finish"

--- Part3.py
+++ Part3.py
@@ -1,6 +1,7 @@
 import pickle
 import sys
 import os
+from nltk.stem import PorterStemmer
 
 # Load Inverted Index and Term Info pickles
 
@@ -15,11 +16,17 @@ with open('Term-Info.pickle', 'rb') as handle:
 argv = sys.argv
 method = argv[1]
 term = argv[2]
+s = term
+
+ps = PorterStemmer()
+
+term = ps.stem(term)
+term = term.replace("'", "")
 
 # Answer Query
 
 if method == "--term":
-    print("Listing for term : " + str(term))
+    print("Listing for term : " + str(s))
     print("TERM-ID : " + str(b[term][0]))
     print("Number of documents containing term : " + str(b[term][2]))
     print("Term frequency in corpus : " + str(b[term][1]))
